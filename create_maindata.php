aa8a4952a8d6bd338cc11a71b9efe961610f4c47
unknown <akashbachhania@gmail.com>
2014-12-01 17:37:02 +0530

    braj commit

--- create_maindata.php
+++ create_maindata.php
@@ -1,20 +1,19 @@
 <?php
- $id=$_REQUEST['id'];
+$id=$_REQUEST['id'];
 include 'include/header.php';
 include 'connection.php';
-
+//echo "<pre>";print_r($labels);die("HELLO");
  $select="SELECT * FROM alog_company where id='$id'";
  $result = mysql_query($select);
  $customer=mysql_fetch_array($result);
- //echo "<pre>";print_r($customer);die("HELLO");
 ?>
    
 <div id="vis-holder"> 
     <div class="vis">
-     <div class="mod-title">Create Maindata</div>
+     <div class="mod-title"><?php echo $labels['create_maindata'];?></div>
      <div class="first_section">
      
-  <label>Cutomer Name:</label>
+  <label><?php echo $labels['customer_name'];?></label>
       <?php //session_start();
         if(isset($_SESSION['customer_name'])){
 			echo $_SESSION['customer_name']=$customer['name_of_company'];
@@ -26,32 +25,32 @@ include 'connection.php';
           <form name="create_customer_first" id="create_customer_first" method="POST" class="customer">
           <table cellpadding="5" cellspacing="5">
             <tr>
-              <td>Name Of Company</td>
+              <td><?php echo $labels['name_of_company'];?></td>
               <td><input type="text" name="name_of_company" id="name_of_company" value="<?php echo $customer['name_of_company'];?>"></td>
             </tr>
 
             <tr>
-              <td>Address</td>
+              <td><?php echo $labels['address'];?></td>
               <td><input type="text" name="address" id="address" value="<?php echo $customer['address'];?>" ></td>
             </tr>
 
             <tr>
-              <td>Contact Person</td>
+              <td><?php echo $labels['contact_person'];?></td>
               <td><input type="text" name="name_of_person" id="name_of_person" value="<?php echo $customer['name_of_contact_person'];?>"></td>
             </tr>
 
             <tr>
-              <td>email</td>
+              <td><?php echo $labels['email'];?></td>
               <td><input type="email" name="email" id="email" value="<?php echo $customer['email'];?>"></td>
             </tr>
 
             <tr>
-              <td>Password</td>
+              <td><?php echo $labels['Password'];?></td>
               <td><input type="password" name="password" id="password" value="<?php echo $customer['password'];?>"></td>
             </tr>
 
             <tr>
-              <td colspan="2">Insert Instruction</td>
+              <td colspan="2"><?php echo $labels['insert_instruction'];?></td>
             </tr>
 
             <tr>
@@ -62,7 +61,7 @@ include 'connection.php';
             </tr>
 
             <tr>
-              <td><input class="customer_button"  onclick="redirect(this);" name="first_section" type="button" value="save"/></td>
+              <td><input class="customer_button"  onclick="redirect(this);" name="first_section" type="button" value="<?php echo $labels['save'];?>"/></td>
               <td></td>
             </tr>
           </table>
